249eee81aa65e0258d198dfbafdae9e7f9d68c26
karolkusmi <karolkusmierz@icloud.com>
2026-01-10 19:14:22 +0100

    Actualizo botones Navbar
    Actualizo botones Navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -4,20 +4,23 @@ export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/signin">
-					<span className="navbar-brand mb-0 h1">Ir a signin</span>
+			<div className="ml-auto">
+				<Link to="/">
+					<button className="btn btn-primary">Log in</button>
 				</Link>
+			</div>
+			<div className="container">
+				<div className="ml-auto">
+					<Link to="/signin">
+						<button className="btn btn-primary">Sign in</button>
+					</Link>
+				</div>
 				<div className="ml-auto">
 					<Link to="/Aboutus">
 						<button className="btn btn-primary">Aboutus</button>
 					</Link>
 				</div>
-				<div className="ml-auto">
-					<Link to="/">
-						<button className="btn btn-primary">Log in</button>
-					</Link>
-				</div>
+
 				<div className="ml-auto">
 					<Link to="/reset-password">
 						<button className="btn btn-primary">Reset password</button>
